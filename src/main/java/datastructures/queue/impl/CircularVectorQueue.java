517fb6d1b6cdb6d641a7dcc8cff81d5b203bc40a
Sergiu Motpan <motpansergiu@gmail.com>
2019-01-15 10:05:09 +0200

    Add solution for the problem 'Implement a queue using a circular array'

---
+++ src/main/java/datastructures/queue/impl/CircularVectorQueue.java
@@ -0,0 +1,107 @@
+package datastructures.queue.impl;
+
+import datastructures.queue.Queue;
+
+
+public class CircularVectorQueue<T> implements Queue<T> {
+
+    private int head;
+    private int tail;
+    private int size;
+    private Object [] elements;
+
+    public CircularVectorQueue(int size) {
+        this.elements = new Object[size];
+        this.size = size;
+        this.head = -1;
+        this.tail = -1;
+    }
+
+    @Override
+    public void enqueue(T value) {
+
+        if (isFull()) {
+            System.out.println("Queue is full!");
+            return;
+        }
+
+        if (isEmpty()) {
+            head = 0;
+            tail = 0;
+            elements[tail] = value;
+            return;
+        }
+
+        if (tail == (size - 1)) {
+            tail = 0;
+            elements[tail] = value;
+            return;
+        }
+
+        elements[++tail] = value;
+    }
+
+    private boolean isFull() {
+        return (head == 0 && tail == (size - 1)) || (tail == (head - 1));
+    }
+
+    @Override
+    public T dequeue() {
+
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return null;
+        }
+
+        if (head == tail) {
+            T value = (T) elements[head];
+            head = -1;
+            tail = -1;
+            return value;
+        }
+
+
+        if (head == (size - 1)) {
+            T value = (T) elements[head];
+            head = 0;
+            return value;
+        }
+
+        T value = (T) elements[head];
+        ++ head;
+
+        return value;
+    }
+
+    @Override
+    public T front() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        return (T) elements[head];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (head == -1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{\n");
+        sb.append("head = ").append(head).append("\n");
+        sb.append("tail = ").append(tail).append("\n");
+        sb.append("[ ");
+
+        for (int i = 0; i < elements.length; ++i) {
+            sb.append(elements[i]).append(" ");
+        }
+        sb.append("]\n}");
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/datastructures/queue/impl/CircularVectorQueueTest.java
@@ -0,0 +1,64 @@
+package datastructures.queue.impl;
+
+import datastructures.queue.Queue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularVectorQueueTest {
+
+    private static final int SIZE = 5;
+    private Queue<Integer> queue;
+
+    @BeforeEach
+    public void init() {
+        queue = new CircularVectorQueue<>(SIZE);
+    }
+
+    @Test
+    void enqueueAndDequeue() {
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        assertEquals(new Integer(10), queue.dequeue());
+        assertEquals(new Integer(20), queue.dequeue());
+        assertEquals(new Integer(30), queue.dequeue());
+        assertEquals(new Integer(40), queue.dequeue());
+        assertEquals(new Integer(50), queue.dequeue());
+    }
+
+    @Test
+    void enqueueAndDequeueMixedOperations() {
+        queue.enqueue(30);
+        queue.enqueue(20);
+        queue.enqueue(10);
+        queue.enqueue(100);
+        queue.enqueue(200);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(1000);
+
+        assertEquals(new Integer(10), queue.dequeue());
+        assertEquals(new Integer(100), queue.dequeue());
+        assertEquals(new Integer(200), queue.dequeue());
+        assertEquals(new Integer(1000), queue.dequeue());
+    }
+
+    @Test
+    void front() {
+        queue.enqueue(30);
+        queue.enqueue(20);
+        queue.dequeue();
+
+        assertEquals(new Integer(20), queue.front());
+    }
+
+    @Test
+    void isEmpty() {
+        assertTrue(queue.isEmpty());
+    }
+}
